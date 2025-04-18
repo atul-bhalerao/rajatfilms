e0131d782ac5b33416ef97bc945dd3d68e2c504e
Atul Bhalerao <56407799+atul-bhalerao@users.noreply.github.com>
2025-04-18 10:54:23 +0530

    small fixes done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,89 @@
-$(document).ready(function () {
-    let $slider = $('.projectSlider');
-  
-    $slider.slick({
-      centerMode: true,
-      variableWidth: true,
-      arrows: true,
-      slidesToShow: 1, // required but overridden by variableWidth
-      initialSlide: 2, // ensure centering works better
+jQuery(document).ready(function(){
+
+  jQuery('.projectSlider').slick({
+      dots: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 1,
       responsive: [
         {
-          breakpoint: 768,
+          breakpoint: 1024,
           settings: {
-            arrows: true,
-            centerMode: true,
-            variableWidth: true,
-            slidesToShow: 1
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
           }
         },
         {
           breakpoint: 480,
           settings: {
-            arrows: true,
-            centerMode: true,
-            variableWidth: true,
-            slidesToShow: 1
+            slidesToShow: 1,
+            slidesToScroll: 1
           }
         }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
       ]
     });
-  
-    // Force re-initialization if you have images that load after DOM ready
-    $slider.slick('setPosition');
-  });
+
+    jQuery('.blogSlider').slick({
+      dots: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+  //   Trusted by
+    const marquee = document.getElementById("marquee");
+    let position = 0;
+
+    function scrollMarquee() {
+      position -= 1; // speed
+      if (Math.abs(position) > marquee.offsetWidth) {
+        position = marquee.parentElement.offsetWidth;
+      }
+      marquee.style.transform = `translateX(${position}px)`;
+      requestAnimationFrame(scrollMarquee);
+    }
+
+    scrollMarquee();
+                    
+});
