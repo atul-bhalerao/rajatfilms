679fc5703c697788027cc1391590627368a3c1ce
Atul Bhalerao <56407799+atul-bhalerao@users.noreply.github.com>
2025-04-19 11:08:26 +0530

    mobile and loader added

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,23 +8,7 @@ jQuery(document).ready(function(){
       slidesToScroll: 1,
       responsive: [
         {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            infinite: true,
-            dots: false
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
+          breakpoint: 992,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1
@@ -44,23 +28,7 @@ jQuery(document).ready(function(){
       slidesToScroll: 1,
       responsive: [
         {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: false
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
+          breakpoint: 992,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1
@@ -72,18 +40,94 @@ jQuery(document).ready(function(){
       ]
     });
   //   Trusted by
-    const marquee = document.getElementById("marquee");
-    let position = 0;
+  jQuery('.marquee').marquee({
 
-    function scrollMarquee() {
-      position -= 1; // speed
-      if (Math.abs(position) > marquee.offsetWidth) {
-        position = marquee.parentElement.offsetWidth;
-      }
-      marquee.style.transform = `translateX(${position}px)`;
-      requestAnimationFrame(scrollMarquee);
-    }
+    // Set to false if you want to use jQuery animate method
+    allowCss3Support: true,
+  
+    // CSS3 easing function
+    css3easing: 'linear',
+  
+    // Requires jQuery easing plugin.
+    easing: 'linear',
+  
+    // Time to wait before starting the animation
+    delayBeforeStart: 1000,
+  
+    // 'left', 'right', 'up' or 'down'
+    direction: 'left',
+  
+    // Should the marquee be duplicated to show an effect of continues flow
+    duplicated: 1,
+  
+    // number of duplicates to create, default is 1
+    duplicateCount: true,
+  
+    // Duration of the animation
+    duration: 18000,
+  
+    // Space in pixels between the tickers
+    gap: 0,
+  
+    // On cycle pause the marquee
+    pauseOnCycle: false,
+  
+    // Pause on hover
+    pauseOnHover: false,
+  
+    // The marquee is visible initially positioned next to the border towards it will be moving
+    startVisible: true
+    
+  });
+              
+  // Page Loader
 
-    scrollMarquee();
-                    
+// Custom ease
+const easeExpoOut = "cubic-bezier(0.33, 1, 0.68, 1)";
+
+// White overlay animation
+gsap.to(".whiteOverlay", {
+  top: 0,
+  height: 0,
+  delay: 0.6, // 1600ms
+  duration: 0.5, // 1000ms
+  ease: easeExpoOut
+});
+
+// Blue overlay animation
+gsap.to(".blueOverlay", {
+  top: 0,
+  height: 0,
+  delay: 0.8, // 1800ms
+  duration: 0.5, // 1000ms
+  ease: easeExpoOut
+});
+//
+// Heading animation
+gsap.to(".heroBanner h3", {
+  top: 0,
+  delay: 1.2, // 1600ms
+  duration: 0.5, // 1000ms
+  ease: easeExpoOut
+});
+
+gsap.to(".heroBanner .heroVideo", {
+  top: 0,
+  delay: 1.5, // 1600ms
+  duration: 0.5, // 1000ms
+  ease: easeExpoOut
+});
+
+// Banner animation
+gsap.to(".bannerRight", {
+  width: "100%",
+  delay: 2, // 2000ms
+  duration: 1, // 1000ms
+  ease: easeExpoOut
+});
+
+setTimeout(function () {
+  jQuery('.loading').remove();
+}, 2000);
+
 });
